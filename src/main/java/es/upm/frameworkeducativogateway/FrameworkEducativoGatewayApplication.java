1e3bc60ee9c1321eb2ed9f76da168b0b80a15a7a
tronxi <sergio.gsanchez97@gmail.com>
2019-10-27 22:31:12 +0100

    added cors configuration

--- src/main/java/es/upm/frameworkeducativogateway/FrameworkEducativoGatewayApplication.java
+++ src/main/java/es/upm/frameworkeducativogateway/FrameworkEducativoGatewayApplication.java
@@ -3,6 +3,29 @@ package es.upm.frameworkeducativogateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.NettyWriteResponseFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +33,65 @@ public class FrameworkEducativoGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FrameworkEducativoGatewayApplication.class, args);
+    }
+
+    @Configuration
+    public class CorsConfiguration {
+
+        private static final String ALLOWED_HEADERS = "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN,token,username,client";
+        private static final String ALLOWED_METHODS = "*";
+        private static final String ALLOWED_ORIGIN = "*";
+        private static final String ALLOWED_EXPOSE = "*";
+        private static final String MAX_AGE = "3600";
+
+        @Bean
+        public WebFilter corsFilter() {
+            return (ServerWebExchange ctx, WebFilterChain chain) -> {
+                ServerHttpRequest request = ctx.getRequest();
+                if (CorsUtils.isCorsRequest(request)) {
+                    ServerHttpResponse response = ctx.getResponse();
+                    HttpHeaders headers = response.getHeaders();
+                    headers.set("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
+                    headers.add("Access-Control-Allow-Methods", ALLOWED_METHODS);
+                    headers.add("Access-Control-Max-Age", MAX_AGE);
+                    headers.add("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+                    headers.add("Access-Control-Expose-Headers", ALLOWED_EXPOSE);
+                    headers.add("Access-Control-Allow-Credentials", "true");
+                    if (request.getMethod() == HttpMethod.OPTIONS) {
+                        response.setStatusCode(HttpStatus.OK);
+                        return Mono.empty();
+                    }
+                }
+                return chain.filter(ctx);
+            };
+        }
+    }
+
+
+    @Component("corsResponseHeaderFilter")
+    public class CorsResponseHeaderFilter implements GlobalFilter, Ordered {
+
+        @Override
+        public int getOrder() {
+            return NettyWriteResponseFilter.WRITE_RESPONSE_FILTER_ORDER + 1;
+        }
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            return chain.filter(exchange).then(Mono.defer(() -> {
+                exchange.getResponse().getHeaders().entrySet().stream()
+                        .filter(kv -> (kv.getValue() != null && kv.getValue().size() > 1))
+                        .filter(kv -> (kv.getKey().equals(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)
+                                || kv.getKey().equals(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS)))
+                        .forEach(kv -> {
+                            kv.setValue(new ArrayList<String>() {{
+                                add(kv.getValue().get(0));
+                            }});
+                        });
+
+                return chain.filter(exchange);
+            }));
+        }
     }
 
 }
